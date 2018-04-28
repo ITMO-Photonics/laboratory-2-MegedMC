580e7fb4e94e567fae8deb74fec983a3933fafdd
MegedMC <rem3095@yandex.ru>
2018-04-28 14:48:09 +0300

    completed:

--- particle-animation.py
+++ particle-animation.py
@@ -6,17 +6,18 @@ circle, = ax.plot([], [],'rs', ms=15)
 coord = np.array([20.,90.,10.,0.])      #третья координата - вертикальная скорость
 def init():
     ax.set_xlim([0., 100.])
-    ax.set_ylim([-10., 100.])
+    ax.set_ylim([0., 100.])
     return circle,
 
 border=2.5
 g = -9.8
 h = 1./50.
-def eiler(coord,h,g,border):            #явный метод Эйлера
+deg=0.9                                     #значение диссипации скорости
+def eiler(coord,h,g,border,deg):            #явный метод Эйлера
     coord[0] = coord[0] + h * coord[2]
     buf = coord[1] + h * coord[3]
     if buf < border:
-        coord[3] = - coord[3] * 0.75
+        coord[3] = - coord[3] * deg
         coord[1]=border
     else:
         coord[1]=buf
@@ -25,12 +26,12 @@ def eiler(coord,h,g,border):            #явный метод Эйлера
         coord[2] = -coord[2]
     return coord
 ##############################
-def n_eiler(coord,h,g,border):          #неявный метод Эйлера
+def n_eiler(coord,h,g,border,deg):          #неявный метод Эйлера
     coord[0] = coord[0] + h * coord[2]
     coord[3] = coord[3] + h * g
     buf = coord[1] + h * coord[3]
     if buf < border:
-        coord[3] = - coord[3] * 0.75
+        coord[3] = - coord[3] * deg
         coord[1]=border
     else:
         coord[1]=buf
@@ -38,12 +39,12 @@ def n_eiler(coord,h,g,border):          #неявный метод Эйлера
         coord[2] = -coord[2]
     return coord
 ##############################
-def R_G(coord,h,g,border):          #метод Рунге-Кутты
+def R_G(coord,h,g,border,deg):          #метод Рунге-Кутты
     coord[0] = coord[0] + h * coord[2]
     buf3 = coord[3] + h * g
     buf1 = coord[1] + h * (coord[3] + buf3) / 2.
     if buf1 < border:
-        buf3 = - buf3 * 0.85
+        buf3 = - buf3 * deg
         coord[1]=border
     else:
         coord[1] = buf1
@@ -54,8 +55,8 @@ def R_G(coord,h,g,border):          #метод Рунге-Кутты
 ##############################
 
 def updatefig(frame):
-    eiler(coord,h,g,border)
-    #R_G(coord,h,g,border)
+    #eiler(coord,h,g,border,deg)
+    R_G(coord,h,g,border,deg)
     circle.set_xdata(coord[0])
     circle.set_ydata(coord[1])
     return circle,
